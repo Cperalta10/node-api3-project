f44955ea61cf933bb37b29583476a541263b224e
Christopher Peralta <chrisperalta1998@gmail.com>
2022-08-11 00:01:50 -0700

    finished 2 request handlers

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -14,7 +14,7 @@ async function validateUserId(req, res, next) {
   try {
     const user = await User.getById(req.params.id);
     if (!user) {
-      res.status(404).json({ message: "user does not exist" });
+      res.status(404).json({ message: "not found" });
     } else {
       req.user = user;
       next();
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -25,11 +25,18 @@ router.get("/", (req, res) => {
 router.get("/:id", validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.json(req.user);
 });
 
-router.post("/", validateUser, (req, res) => {
+router.post("/", validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  userModel
+    .insert({ name: req.name })
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
 });
 
 router.put("/:id", validateUserId, validateUser, (req, res) => {
