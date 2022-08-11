cc04db6bf257c7ea98e632e46ba4c114d5260783
Christopher Peralta <chrisperalta1998@gmail.com>
2022-08-10 23:47:21 -0700

    finished middleware

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const port = 9000;
+
+const server = require("./api/server");
+
+server.listen(port, () => {
+  console.log("server running on http://localhost:9000");
+});
